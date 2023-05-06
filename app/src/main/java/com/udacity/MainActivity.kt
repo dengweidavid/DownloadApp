b620772b3cdb6eff537560a03be8e43ad4fe51ab
dengweidavid <wei.deng@machineswithvision.com>
2023-05-06 11:11:42 +0100

    Add radio buttons and download cloud image in start frame

--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -9,6 +9,8 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import kotlinx.android.synthetic.main.activity_main.*
@@ -18,6 +20,7 @@ import kotlinx.android.synthetic.main.content_main.*
 class MainActivity : AppCompatActivity() {
 
     private var downloadID: Long = 0
+    private var url: String = ""
 
     private lateinit var notificationManager: NotificationManager
     private lateinit var pendingIntent: PendingIntent
@@ -30,8 +33,8 @@ class MainActivity : AppCompatActivity() {
 
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
-        custom_button.setOnClickListener {
-            download()
+        loading_button.setOnClickListener {
+            download(url)
         }
     }
 
@@ -41,9 +44,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun download() {
+    private fun download(url: String) {
         val request =
-            DownloadManager.Request(Uri.parse(URL))
+            DownloadManager.Request(Uri.parse(url))
                 .setTitle(getString(R.string.app_name))
                 .setDescription(getString(R.string.app_description))
                 .setRequiresCharging(false)
@@ -55,9 +58,37 @@ class MainActivity : AppCompatActivity() {
             downloadManager.enqueue(request)// enqueue puts the download request in the queue.
     }
 
+    fun onRadioButtonClicked(view: View) {
+        if (view is RadioButton) {
+            val isChecked = view.isChecked
+            when (view.getId()) {
+                R.id.glide_button ->
+                    if (isChecked) {
+                        url = GLIDE_URL
+                    }
+
+                R.id.load_app_button ->
+                    if (isChecked) {
+                        url = LOAD_APP_URL
+                    }
+
+                R.id.retrofit_button -> {
+                    if (isChecked) {
+                        url = RETROFIT_URL
+                    }
+                }
+            }
+        }
+    }
+
     companion object {
-        private const val URL =
+        private const val GLIDE_URL =
+            "https://github.com/bumptech/glide/archive/refs/heads/master.zip"
+        private const val LOAD_APP_URL =
             "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+        private const val RETROFIT_URL =
+            "https://github.com/square/retrofit/archive/heads/master.zip"
+
         private const val CHANNEL_ID = "channelId"
     }
 
